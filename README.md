b045560af5837eb8fe0d8706440285b2a47bc8eb
Breebee <surfluff@duck.com>
2024-12-08 21:34:26 -0700

    1.1.0 commit

--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 A simple webfishing mod that adds zoosexual cosmetics and titles
 
 Depends on Lure https://thunderstore.io/c/webfishing/p/Sulayre/Lure/
+
+# changelog
+
+1.1.0
+- Cynophilia shirt
+- Zootherian hat
+- Therian title
+- Recolored some icons
+
+1.0.0
+- initial commit
+- Zooey title
+- Dog kisser title
+- Cat kisser title
+- Zoosexual pride shirt
--- main.gd
+++ main.gd
@@ -11,3 +11,6 @@ func _ready():
 	Lure.add_content(id, "title_dog_kisser", "mod://Resources/Cosmetics/Titles/title_dog_kisser.tres", [Lure.LURE_FLAGS.FREE_UNLOCK])
 	Lure.add_content(id, "title_cat_kisser", "mod://Resources/Cosmetics/Titles/title_cat_kisser.tres", [Lure.LURE_FLAGS.FREE_UNLOCK])
 	Lure.add_content(id, "title_zooey", "mod://Resources/Cosmetics/Titles/title_zooey.tres", [Lure.LURE_FLAGS.FREE_UNLOCK])
+	Lure.add_content(id, "undershirt_paw", "mod://Resources/Cosmetics/Undershirts/undershirt_paw.tres", [Lure.FLAGS.FREE_UNLOCK])
+	Lure.add_content(id, "baseball_cap_zoother", "mod://Resources/Cosmetics/Hats/baseball_cap_zoother.tres", [Lure.FLAGS.FREE_UNLOCK])
+	Lure.add_content(id, "title_therian", "mod://Resources/Cosmetics/Titles/title_therian.tres", [Lure.LURE_FLAGS.FREE_UNLOCK])
